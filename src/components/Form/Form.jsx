8e1be6e1e20bf0e657dd65712f7847808792914a
Alina_Kuzyk <dr.alinakuzyk@gmail.com>
2023-02-14 14:27:14 +0100

    fixed validation

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -10,8 +10,7 @@ import {
 import { useDispatch } from 'react-redux';
 import { addContact } from 'redux/contacts/operations';
 
-const phoneRegExp =
-  /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/;
+const phoneRegExp = /^\+(?:[0-9] ?){6,14}[0-9]$/;
 
 const validationSchemeForm = yup.object().shape({
   name: yup
